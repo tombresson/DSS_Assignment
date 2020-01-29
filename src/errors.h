f8f8ce0f9a66cf0c62da4998f68a422f8ff04288
Tom Bresson <thomas.bresson@fbgpg.com>
2020-01-29 11:39:38 -0500

    Change error enum definition

--- src/errors.h
+++ src/errors.h
@@ -44,8 +44,9 @@
 
 typedef enum
 {
-    APP_NO_ERROR = 0,
-    APP_JSON_DATA_UNABLE_TO_BE_RETRIVED,
+    APPERR_NO_ERROR = 0,
+    APPERR_JSON_DATA_UNABLE_TO_BE_RETRIVED,
+    APPERR_UNABLE_TO_ALLOCATE_MEMORY
 }appErrors_t;
 
 /* ****************************   Structures   **************************** */
